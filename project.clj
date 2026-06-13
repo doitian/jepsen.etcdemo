6c229f7a5bc91ac1b6b9ceb07ede1b020879ae02
ian <me@iany.me>
2026-06-13 12:57:00 +0800

    add jepsen test harness and update project dependencies
    Introduce Jepsen testing infrastructure by adding necessary dependencies,
    a main entry point for running tests, and configuration via mise.toml.
    Also ignore node and store directories to prevent accidental commits.

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :main jepsen.etcdemo
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [jepsen "0.3.11"]
+                 [verschlimmbesserung "0.1.0"]]
   :repl-options {:init-ns jepsen.etcdemo})
--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -1,6 +1,17 @@
-(ns jepsen.etcdemo)
+(ns jepsen.etcdemo
+  (:require [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn etcd-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         {:pure-generators true}
+         opts))
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
+  [& args]
+  (cli/run! (cli/single-test-cmd {:test-fn etcd-test}) args))
