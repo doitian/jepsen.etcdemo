8cb01d91b681391a27738a1e77a465d874af7646
ian <me@iany.me>
2026-06-14 20:10:31 +0800

    add etcd daemon management and cluster configuration helpers
    Introduce helper functions to construct peer/client URLs and initial
    cluster strings, and update the DB setup to start/stop etcd as a daemon
    with appropriate configuration. This enables proper cluster formation
    and process lifecycle management during tests.

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -11,6 +11,32 @@
    [jepsen.os.debian :as debian]))
 
 (def dir "/opt/etcd")
+(def binary "etcd")
+(def logfile (str dir "/etcd.log"))
+(def pidfile (str dir "/etcd.pid"))
+
+(defn node-url
+  "An HTTP URL for connecting to a node on a particular port."
+  [node port]
+  (str "http://" node ":" port))
+
+(defn peer-url
+  "The HTTP URL for other peers to talk to a node."
+  [node]
+  (node-url node 2380))
+
+(defn client-url
+  "The HTTP URL clients use to talk to a node."
+  [node]
+  (node-url node 2379))
+
+(defn initial-cluster
+  "Constructs an initial cluster string for a test, like
+  \"foo=foo:2380,bar=bar:2380,...\""
+  [test]
+  (->> (:nodes test)
+       (map (fn [node] (str node "=" (peer-url node))))
+       (str/join ",")))
 
 (defn db
   "Etcd DB for a particular version."
@@ -22,10 +48,29 @@
       (c/su
        (let [url (str "https://storage.googleapis.com/etcd/" version
                       "/etcd-" version "-linux-amd64.tar.gz")]
-         (cu/install-archive! url dir))))
+         (cu/install-archive! url dir))
+
+       (cu/start-daemon!
+        {:logfile logfile
+         :pidfile pidfile
+         :chdir dir}
+        binary
+        :--log-output :stderr
+        :--name (name node)
+        :--listen-peer-urls (peer-url node)
+        :--listen-client-urls (client-url node)
+        :--advertise-client-urls (client-url node)
+        :--initial-cluster-state :new
+        :--initial-advertise-peer-urls (peer-url node)
+        :--initial-cluster (initial-cluster test))
+
+       (Thread/sleep 10000)))
 
     (teardown! [_ test node]
-      (info node "tearing down etcd"))))
+      (info node "tearing down etcd")
+
+      (cu/stop-daemon! binary pidfile)
+      (c/su (c/exec :rm :-rf dir)))))
 
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
