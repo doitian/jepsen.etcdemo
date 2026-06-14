142ec58da4d138a5fc3fd369a70e626f32da7579
ian <me@iany.me>
2026-06-14 17:22:50 +0800

    add support for both running tests and serving results via CLI
    Merge the serve command into the CLI options so users can either run a
    test or start a web server to browse results, instead of being limited
    to only running tests.

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -14,4 +14,4 @@
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
   [& args]
-  (cli/run! (cli/single-test-cmd {:test-fn etcd-test}) args))
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test}) (cli/serve-cmd)) args))
